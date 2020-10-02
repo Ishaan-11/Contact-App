616d1c52ec349e053e2cf29344675ff7003ce610
Ishaan-11 <ishaansharma1998@gmail.com>
2020-10-02 17:11:07 +0530

    Use map to remove repetition App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,29 +3,24 @@ import Card from "./Card";
 import Contacts from "../contacts";
 import Avatar from "./Avatar";
 
+function createCard(contact){
+  return(
+    <Card
+      key={contact.id}
+      id={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
 function App() {
   return (
     <div>
         <h1 className="heading">My Contacts</h1>
-        <Avatar img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRCd8uzUORlwWw-Z_tM_k1Co0dtChFnmtM4lw&usqp=CAU" />
-        <Card
-          name={Contacts[0].name}
-          img={Contacts[0].imgURL}
-          tel={Contacts[0].phone}
-          email={Contacts[0].email}
-        />
-        <Card
-          name={Contacts[1].name}
-          img={Contacts[1].imgURL}
-          tel={Contacts[1].phone}
-          email={Contacts[1].email}
-        />
-        <Card
-          name={Contacts[2].name}
-          img={Contacts[2].imgURL}
-          tel={Contacts[2].phone}
-          email={Contacts[2].email}
-        />
+        <Avatar img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRCd8uzUORlwWw-Z_tM_k1Co0dtChFnmtM4lw&usqp=CAU"  />
+        {Contacts.map(createCard)}
     </div>
   );
 }
